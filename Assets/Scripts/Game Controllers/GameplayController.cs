138320009e6948940d5f296f04fd298d9b0d4a4e
ChandanSuri <csuri21296@gmail.com>
2016-10-24 02:01:53 +0530

    Scripted Gameplay Controller, Game is Fully Functional

--- Assets/Scripts/Game Controllers/GameplayController.cs
+++ Assets/Scripts/Game Controllers/GameplayController.cs
@@ -1,15 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameplayController : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject pausePanel;
+    [SerializeField]
+    private Button restartGameButton;
+    [SerializeField]
+    private Text scoreText, pauseText;
+
+    private int score;
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        scoreText.text = score.ToString() + "M";
+        StartCoroutine(CountScore());
+    }
+
+    IEnumerator CountScore()
+    {
+        yield return new WaitForSeconds(0.6f);
+        score++;
+        scoreText.text = score.ToString() + "M";
+        StartCoroutine(CountScore());
+    }
+
+    void OnEnable()
+    {
+        PlayerDied.endGame += PlayerDiedEndTheGame;
+    }
+
+    void OnDisable()
+    {
+        PlayerDied.endGame -= PlayerDiedEndTheGame;
+    }
+
+    void PlayerDiedEndTheGame()
+    {
+        if (!PlayerPrefs.HasKey("Score"))
+        {
+            PlayerPrefs.SetInt("Score", 0);
+        }else
+        {
+            int highscore = PlayerPrefs.GetInt("Score");
+            if (score > highscore)
+            {
+                PlayerPrefs.SetInt("Score", score);
+            }
+        }
+        pauseText.text = "Game Over";
+        pausePanel.SetActive(true);
+        restartGameButton.onClick.RemoveAllListeners();
+        restartGameButton.onClick.AddListener(() => RestartGame());
+        Time.timeScale = 0f;
+
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0f;
+        pausePanel.SetActive(true);
+        restartGameButton.onClick.RemoveAllListeners();
+        restartGameButton.onClick.AddListener(() => ResumeGame());
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1f;
+        Application.LoadLevel("MainMenu");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        Application.LoadLevel("Gameplay");
+    }
 }
